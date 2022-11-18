afa360f8718f0cc0338f39ee0f8cc3744865861c
salah-abukhatwa <salah.abukhatwa@gmail.com>
2022-11-18 12:30:14 +0300

    added setting-box

--- JS/master.js
+++ JS/master.js
@@ -1,3 +1,25 @@
+
+
+
+
+// Toggle Spin Class On Icon
+document.querySelector(".toggle-setting .fa-gear").onclick = function () {
+    // Toggle Class Fa-spin For Rotation on Self 
+    this.classList.toggle("fa-spin")
+
+     // Toggle Class opean ON  Main  Settings Box 
+    document.querySelector(".setting-box").classList.toggle("opean")
+};
+
+// Switch colors
+const colorsli = document.querySelectorAll(".colors-list li")
+colorsli.forEach(li => {
+    li.addEventListener("click", (e) => {
+        console.log(e.target.dataset.color)
+        // Set color on root
+        document.documentElement.style.setProperty("--main-color", e.target.dataset.color)
+    });
+});
 // select landing page
 let landingPage = document.querySelector(".landing-page");
 
