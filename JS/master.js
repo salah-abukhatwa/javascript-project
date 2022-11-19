9d2a13295c3345c62bdc03f270064f69266b0341
salah-abukhatwa <salah.abukhatwa@gmail.com>
2022-11-19 21:19:34 +0300

    added option box color _code JS and css

--- JS/master.js
+++ JS/master.js
@@ -1,23 +1,42 @@
+// Check If there is local Storage Color Option
+let mainColors = localStorage.getItem("color_option");
 
+if (mainColors !== null) {
 
+    document.documentElement.style.setProperty("--main-color", mainColors);
+    // Remove Active Class From All color list item
+     document.querySelectorAll(".colors-list li").forEach(element => {
+         element.classList.remove("active")
+          // add Active Class
+    if (element.dataset.color === mainColors) {
+        element.classList.add("active")}
+     });
+   
+}
 
 
 // Toggle Spin Class On Icon
 document.querySelector(".toggle-setting .fa-gear").onclick = function () {
     // Toggle Class Fa-spin For Rotation on Self 
-    this.classList.toggle("fa-spin")
+    this.classList.toggle("fa-spin");
 
      // Toggle Class opean ON  Main  Settings Box 
-    document.querySelector(".setting-box").classList.toggle("opean")
+    document.querySelector(".setting-box").classList.toggle("opean");
 };
 
 // Switch colors
-const colorsli = document.querySelectorAll(".colors-list li")
+const colorsli = document.querySelectorAll(".colors-list li");
 colorsli.forEach(li => {
     li.addEventListener("click", (e) => {
-        console.log(e.target.dataset.color)
         // Set color on root
-        document.documentElement.style.setProperty("--main-color", e.target.dataset.color)
+        document.documentElement.style.setProperty("--main-color", e.target.dataset.color);
+        localStorage.setItem("color_option", e.target.dataset.color);
+        // Remove active class from all items
+        e.target.parentElement.querySelectorAll(".active").forEach(element => {
+            element.classList.remove("active")
+        });
+        // Add Active Class on
+        e.target.classList.add("active")
     });
 });
 // select landing page
